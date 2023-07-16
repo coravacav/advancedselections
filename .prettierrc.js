d996fe5c993f5a38bb155bdcd6ef69b75a538356
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-16 17:26:56 -0600

    prettier and start writing first command

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    singleQuote: true,
+    tabWidth: 4,
+    semi: true,
+    trailingComma: 'es5',
+    printWidth: 120,
+    endOfLine: 'lf',
+    arrowParens: 'always',
+    singleAttributePerLine: true,
+};
